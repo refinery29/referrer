ec627d380d8c004647ec9c0ecd78def211e043e7
Yaroslaff Fedin <yaroslaff.fedin@refinery29.com>
2013-03-19 02:23:47 +0800

    Add testling config

---
+++ Specs/Testling.js
@@ -0,0 +1,10 @@
+{
+  "testling" : {
+    "scripts": [
+      "tests/lib/jasmine/jasmine.js",
+      "tests/lib/jasmine/jasmine-tap.js",
+      "tests/specs/*.js",
+      "tests/runner.js"
+    ]
+  }
+}
